33d4dc7fa050f8bb9c0340cba7821e0761f22716
langmeng <lang.meng@uni-colour.com>
2017-06-18 22:30:25 +0800

    自助机列表界面添加显示二维码的对话框。

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -29,3 +29,29 @@ function viewPriceList(event) {
 function createOrEditPriceList(event) {
     return showPostFormModal(event, 'editPriceListForm', null, true);
 }
+
+//显示二维码
+function showPrintStationQrCode(event) {
+    var source = event.target || event.srcElement;
+    var qrCode = $(source).data('qrcode');
+
+    var content = '<div class="modal-content">\
+             <div class="modal-header">\
+                 <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>\
+                 <h4 class="modal-title">微信二维码</h4>\
+             </div>\
+             <div class="modal-body">\
+                <div id="qrCodeDiv"></div>\
+             </div>\
+             <div class="modal-footer">\
+                 <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>\
+             </div>\
+         </div>';
+    $('#modalTemplate .modal-dialog').empty().append(content)
+
+    new QRCode(document.getElementById("qrCodeDiv"), qrCode);
+
+    $('#modalTemplate').modal('show');
+
+    return false;
+}
