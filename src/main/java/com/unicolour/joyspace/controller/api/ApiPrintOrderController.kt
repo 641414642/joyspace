955a1e0f99683f024bd470853a7f6ff23c557018
langmeng <lang.meng@uni-colour.com>
2017-07-12 15:57:06 +0800

    打印请求结果。

--- src/main/java/com/unicolour/joyspace/controller/api/ApiPrintOrderController.kt
+++ src/main/java/com/unicolour/joyspace/controller/api/ApiPrintOrderController.kt
@@ -5,6 +5,7 @@ import com.unicolour.joyspace.dto.CreateOrderRequestResult
 import com.unicolour.joyspace.dto.OrderInput
 import com.unicolour.joyspace.exception.ProcessException
 import com.unicolour.joyspace.service.PrintOrderService
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 class ApiPrintOrderController {
+    private val logger = LoggerFactory.getLogger(ApiPrintOrderController::class.java)
+
     @Autowired
     lateinit var printOrderService: PrintOrderService
 
@@ -19,9 +22,17 @@ class ApiPrintOrderController {
     fun uploadImage(@RequestBody orderInput: OrderInput) : ResponseEntity<CreateOrderRequestResult> {
         try {
             val order = printOrderService.createOrder(orderInput)
-            val params = printOrderService.startPayment(order.id)
+            try {
+                val params = printOrderService.startPayment(order.id)
+                logger.info(params.toString())
+                logger.error(params.toString())
 
-            return ResponseEntity.ok(CreateOrderRequestResult(params))
+                return ResponseEntity.ok(CreateOrderRequestResult(params))
+            }
+            catch (ex: Exception) {
+                ex.printStackTrace()
+                return ResponseEntity.ok(CreateOrderRequestResult(null, 0, null))
+            }
         } catch(e: ProcessException) {
             e.printStackTrace()
             return ResponseEntity.ok(CreateOrderRequestResult(e.errcode, e.message))
--- src/main/java/com/unicolour/joyspace/dto/WxPayParams.kt
+++ src/main/java/com/unicolour/joyspace/dto/WxPayParams.kt
@@ -1,6 +1,6 @@
 package com.unicolour.joyspace.dto
 
-class WxPayParams(
+data class WxPayParams(
     var timeStamp: String, //时间戳从1970年1月1日00:00:00至今的秒数,即当前的时间
     var nonceStr: String, //随机字符串，长度为32个字符以下。
     var pkg: String, //统一下单接口返回的 prepay_id 参数值，提交格式如：prepay_id=*
