9e9e5834393a64b5e38ce5edf9ed38028d3f7549
langmeng <lang.meng@uni-colour.com>
2017-09-20 22:04:57 +0800

    小程序查找距离小于指定值的自助机列表的接口

--- src/main/kotlin/com/unicolour/joyspace/controller/api/ApiPrintStationController.kt
+++ src/main/kotlin/com/unicolour/joyspace/controller/api/ApiPrintStationController.kt
@@ -3,9 +3,11 @@ package com.unicolour.joyspace.controller.api
 import com.unicolour.joyspace.dao.PrintStationDao
 import com.unicolour.joyspace.dto.*
 import com.unicolour.joyspace.model.PrintStation
+import com.unicolour.joyspace.service.GraphQLService
 import com.unicolour.joyspace.service.PrintStationService
 import com.unicolour.joyspace.service.ProductService
 import com.unicolour.joyspace.util.getBaseUrl
+import graphql.GraphQL
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -40,6 +42,44 @@ class ApiPrintStationController {
             return ResponseEntity.ok(ps)
         }
     }
+
+    @Autowired
+    lateinit var graphQLService: GraphQLService
+
+    @RequestMapping("/api/printStation/findByDistance", method = arrayOf(RequestMethod.GET))
+    @ResponseBody
+    fun findByDistance(
+            request: HttpServletRequest,
+            @RequestParam("longitude") longitude: Double,
+            @RequestParam("latitude") latitude: Double,
+            @RequestParam("radius") radius:Int) : Any? {
+        val schema = graphQLService.getGraphQLSchema()
+        val graphQL = GraphQL.newGraphQL(schema).build()
+
+        val query =
+                """
+query {
+	findPrintStationsByDistance(
+            longitude:$longitude,
+            latitude:$latitude,
+            radius:$radius) {
+        printStations {
+                id
+                address
+                longitude
+                latitude
+                wxQrCode
+        }
+	}
+}
+"""
+        val context = hashMapOf<String, Any>( "baseUrl" to getBaseUrl(request))
+        val queryResult = graphQL.execute(query, null, context, emptyMap())
+        val data:Map<String, Any> = queryResult.getData()
+        val result = data["findPrintStationsByDistance"] as? Map<*, *>
+        return result?.get("printStations")
+    }
+
 
     @RequestMapping("/api/printStation/{id}", method = arrayOf(RequestMethod.GET))
     fun findById(request: HttpServletRequest, @PathVariable("id") id: Int) : ResponseEntity<PrintStationDTO> {
