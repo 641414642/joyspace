2ce9804818f77ec40ee364e08a763a609bc43a06
langmeng <lang.meng@uni-colour.com>
2017-12-19 19:06:41 +0800

    后台定时（1分钟一次）更新自助机在线状态

---
+++ src/main/kotlin/com/unicolour/joyspace/service/impl/ScheduledTasks.kt
@@ -0,0 +1,49 @@
+package com.unicolour.joyspace.service.impl
+
+import com.unicolour.joyspace.dao.PrintStationDao
+import com.unicolour.joyspace.dao.PrintStationLoginSessionDao
+import com.unicolour.joyspace.model.PrintStationStatus.OFFLINE
+import com.unicolour.joyspace.model.PrintStationStatus.WORKING
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.util.*
+import javax.transaction.Transactional
+
+
+@Component
+open class ScheduledTasks {
+    @Autowired
+    lateinit var printStationLoginSessionDao: PrintStationLoginSessionDao
+
+    @Autowired
+    lateinit var printStationDao: PrintStationDao
+
+    @Scheduled(initialDelay = 6000, fixedDelay = 6000)  //1分钟更新一次自助机状态
+    @Transactional
+    fun updatePrintStationStatus() {
+        printStationDao.findAll().forEach({
+            if (it.status == WORKING.value || it.status == OFFLINE.value) {
+                var offline = true
+                val printStationLoginSession = printStationLoginSessionDao.findByPrintStationId(it.id)
+                if (printStationLoginSession != null) {
+                    val expireTime = printStationLoginSession.expireTime.timeInMillis
+
+                    val time = Calendar.getInstance()
+                    time.add(Calendar.SECOND, 3600 - 60)
+
+                    if (expireTime > time.timeInMillis) {    //自助机1分钟之内访问过后台
+                        offline = false
+                    }
+                }
+
+                if (offline && it.status == WORKING.value ||
+                        !offline && it.status == OFFLINE.value) {
+                    it.status = if (offline) OFFLINE.value else WORKING.value
+                    printStationDao.save(it)
+                }
+            }
+        })
+    }
+}
+
