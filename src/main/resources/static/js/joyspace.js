6105daae9ad973a7299a947ce613d4ca53658af6
langmeng <lang.meng@uni-colour.com>
2018-02-27 15:09:22 +0800

    修改IE下二维码显示不出来的问题

--- src/main/resources/static/js/joyspace.js
+++ src/main/resources/static/js/joyspace.js
@@ -194,6 +194,32 @@ function showQrCode(event) {
          '</div>';
     $('#modalTemplate .modal-dialog').empty().append(content)
 
+    //IE Object.assign polyfill
+    if (typeof Object.assign != 'function') {
+        Object.assign = function(target, varArgs) { // .length of function is 2
+            'use strict';
+            if (target == null) { // TypeError if undefined or null
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+
+            var to = Object(target);
+
+            for (var index = 1; index < arguments.length; index++) {
+                var nextSource = arguments[index];
+
+                if (nextSource != null) { // Skip over if undefined or null
+                    for (var nextKey in nextSource) {
+                        // Avoid bugs when hasOwnProperty is shadowed
+                        if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                            to[nextKey] = nextSource[nextKey];
+                        }
+                    }
+                }
+            }
+            return to;
+        };
+    }
+
     $('#qrCodeDiv').append(kjua({
        render: 'image',
        crisp: true,
