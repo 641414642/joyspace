d5d5be4b87d6818b7eed7115f08a3b36c228fbba
fangchenxi <mojito515@126.com>
2018-07-05 23:08:23 +0800

    change：用户地址相关接口调整

--- src/main/kotlin/com/unicolour/joyspace/dao/UserAddressDao.kt
+++ src/main/kotlin/com/unicolour/joyspace/dao/UserAddressDao.kt
@@ -2,7 +2,19 @@ package com.unicolour.joyspace.dao
 
 import com.unicolour.joyspace.model.Address
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+
 
 interface UserAddressDao : CrudRepository<Address, Int> {
-    fun findByUserId(userId: Int): List<Address>
+    fun findByUserIdAndDeleted(userId: Int, deleted: Boolean): List<Address>
+
+    @Modifying
+    @Query("update Address a set a.default = false where a.userId = :userId and a.id != :id")
+    fun updateDefault(@Param(value = "userId") userId: Int, @Param(value = "id") id: Int)
+
+    @Modifying
+    @Query("update Address a set a.deleted = true where a.id = :id")
+    fun deleteS(@Param(value = "id") id: Int)
 }
