cfed12c6e8b0c6c89ff1720758a3e73a9e1eb331
fangchenxi <mojito515@126.com>
2018-07-12 22:19:05 +0800

    change:模版查找去掉scene类型

--- db_upgrade.sql
+++ db_upgrade.sql
@@ -118,6 +118,5 @@ UPDATE template_image_info set angle_clip=0.0;
 UPDATE template_image_info set layer_type=2;
 UPDATE template_image_info set type=0;
 
---todo 所有自助机默认支持模板拼图和相册 (后台还无法录入)
 
 ALTER TABLE address ADD deleted BOOLEAN default false NOT NULL;
