dffecdc0d987a1e85fe0cd00656788804ef9dc02
fangchenxi <mojito515@126.com>
2018-07-25 22:36:54 +0800

    bug fix:代码领取优惠券时，代码重复（后台录入没做校验）时，取第一个。

--- src/main/kotlin/com/unicolour/joyspace/dao/CouponDao.kt
+++ src/main/kotlin/com/unicolour/joyspace/dao/CouponDao.kt
@@ -3,13 +3,11 @@ package com.unicolour.joyspace.dao
 import com.unicolour.joyspace.model.Coupon
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.PagingAndSortingRepository
-import org.springframework.data.repository.query.Param
 
 interface CouponDao : PagingAndSortingRepository<Coupon, Int> {
     fun findByIdInOrderByDiscountDesc(idList: List<Int>): List<Coupon>
-    fun findByCodeIgnoreCase(code: String): Coupon?
+    fun findFirstByCodeIgnoreCaseOrderByBeginDesc(code: String): Coupon?
 
     fun findByIdNotIn(couponIds: List<Int>): Iterable<Coupon>
 
